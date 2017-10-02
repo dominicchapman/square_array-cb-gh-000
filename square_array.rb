d243909fbfc28f70796d3f6830e09b5f640dd8ec
Dominic Chapman <dominicchapman@me.com>
2017-10-02 22:16:58 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,13 @@
+# def square_array(array)
+#   squared_array = []
+#   array.each do |i|
+#     squared_array << i ** 2
+#   end
+#   squared_array
+# end
+
 def square_array(array)
-  squared_array = []
-  array.each do |i|
-    squared_array << i ** 2
+  array.collect do |i|
+    i ** 2
   end
-  squared_array
 end
