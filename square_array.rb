de401d56a4b667f6694820295a68c2d1cf59ad62
Dominic Chapman <dominicchapman@me.com>
2017-10-02 22:17:01 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+# def square_array(array)
+#   squared_array = []
+#   array.each do |i|
+#     squared_array << i ** 2
+#   end
+#   squared_array
+# end
+
 def square_array(array)
-  # your code here
+  array.collect do |i|
+    i ** 2
+  end
 end
